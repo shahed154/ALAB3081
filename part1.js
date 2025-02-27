e55688ad3c2577dd4a79841d3a5d51684c94990e
shahed154 <shahedadnan@hotmail.com>
2025-02-26 22:54:45 -0500

    idk anymore man

--- part1.js
+++ part1.js
@@ -47,7 +47,7 @@ const dontDoThis = ((n1 + n2 + n3 + n4) == 50) &&
   (n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4);
 
 
-
+//test
 // HOMEWORK
 
 const divisbleBy5 = (n1 % 5 && n2 % 5  && n3 % 5  && n4 % 5) === 0;
